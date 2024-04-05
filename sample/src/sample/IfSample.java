662e0c9544c402dbfd9937cfc5eb0072d4d71a56
dir-FY24tanizakisota <sota.tanizaki.j728@dirbato.co.jp>
2024-04-05 17:57:48 +0900

    Compare

---
+++ sample/src/sample/IfSample.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class IfSample {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        var score = 70; //成績
+        
+        if (score >= 60) {
+            System.out.println("合格です！");
+            
+         } else {
+             System.out.println("赤点です。。。");
+             
+         }
+
+    }
+
+}
