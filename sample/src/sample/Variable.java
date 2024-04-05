689df3b17c34ce5f0a7c58ac14701be45ed7b0ed
dir-FY24tanizakisota <sota.tanizaki.j728@dirbato.co.jp>
2024-04-05 17:11:53 +0900

    Lesson2-Capter3

---
+++ sample/src/sample/Variable.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        //1日の秒数 = 24時間 × 60分 × 60秒
+        var secondPerDay = 24 * 60 * 60;
+        System.out.println(secondPerDay);
+        
+    }
+
+}
