bd037dd5a7fc41fcc3eb21ca192e04f7b92a076b
dir-FY24tanizakisota <sota.tanizaki.j728@dirbato.co.jp>
2024-04-08 11:48:16 +0900

    最初のコミット

---
+++ sample/src/sample/ForSample.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class ForSample {
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 20; i++) {
+            if(i % 2 == 0) {
+            System.out.println(i);
+            
+        }
+
+    }
+        for(int i = 1;i <= 20; i++) {
+            if(i >20) {
+                //現在の処理をスキップし、次の繰り返しに進む
+                break;
+            }
+            System.out.println(i);
+        }
+        var numbers =new int[] {1,2,3,4,5};
+        
+        for (var number : numbers) {
+            System.out.println(number);
+        }
+
+    }
+}
--- sample/src/sample/IfSample.java
+++ sample/src/sample/IfSample.java
@@ -4,16 +4,17 @@ public class IfSample {
 
     public static void main(String[] args) {
         // TODO 自動生成されたメソッド・スタブ
-        var score = 70; //成績
+        var score = 20; //成績
         
-        if (score >= 60) {
-            System.out.println("合格です！");
-            
-         } else {
+        if (score == 100) {
+            System.out.println("満点です！");
+         } else if (score >= 80){
+             System.out.println("よくできました！");
+         } else if (score >= 60) {
+             System.out.println("合格です！");
+         } else 
              System.out.println("赤点です。。。");
-             
-         }
-
     }
 
-}
+    }
+   
